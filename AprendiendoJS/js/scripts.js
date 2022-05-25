078c35d6492f2021217293fd314c08db8c2f0217
JhovaniVC <velazquezchialex@gmail.com>
2022-05-25 08:29:58 -0500

    Update scripts.js

--- AprendiendoJS/js/scripts.js
+++ AprendiendoJS/js/scripts.js
@@ -1,11 +1,30 @@
+//Object literal
 
+const persona = {
+    nombre: 'Juan',
+    edad: 80,
+    anioNacimiento: function(){
+        return new Date().getFullYear() - this.edad;
+    }
+}
 
+console.log(persona.anioNacimiento());
 
-let viajando = (destino) =>  'Viajando a la ciudad de ' + destino;
+//Object Constructor
+class Tarea{
+    constructor(nombre, urgencia){
+        this.nombre = nombre;
+        this.urgencia = urgencia; 
+    }
+    
+}
 
-
-let viaje; 
-viaje = viajando ('Londres');
-viaje = viajando('Paris'); 
-
-console.log(viaje);
+//crear nuevas tareas 
+const tarea1 = new Tarea('Aprender JavaScript', 'Urgente ');
+const tarea2 = new Tarea('Preparar Café', 'Urgente ');
+const tarea3 = new Tarea('Pasear al perro', 'Media ');
+const tarea4 = new Tarea('Conocer a mis suegros', '0 ');
+console.log(tarea1);
+console.log(tarea2);
+console.log(tarea3);
+console.log(tarea4);
