dd0229d9368e3a5855e1a2342207b052a65f2a8c
edwinperez16 <93944466+edwinperez16@users.noreply.github.com>
2022-05-26 08:20:50 -0500

    siu

--- pagina1/includes/funciones/bd_conexion.php
+++ pagina1/includes/funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost','','','gdlwebcamp');
+    $conn = new mysqli('localhost','root','','gdlwebcamp');
 
     if($conn->connect_error) {
       echo $conn->connect_error;
