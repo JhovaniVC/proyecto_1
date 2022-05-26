4b103f545c03c39080d673520e2fd676cb506c66
JhovaniVC <velazquezchialex@gmail.com>
2022-05-25 23:40:40 -0500

    cambio en el id del index
    uso del fetch en el JS

--- AprendiendoJS/js/scripts.js
+++ AprendiendoJS/js/scripts.js
@@ -1,15 +1,27 @@
-// Destructuring
+descargarUsuarios(30);
 
-const cliente = {
-    nombre: 'Alejandra',
-    tipo: 'Premium'
+
+function descargarUsuarios(cantidad){
+    const api = `https://api.randomuser.me/?nat=US&results=${cantidad} `;
+
+    //llamado a fetch 
+    fetch(api)
+        .then(respuesta => respuesta.json())
+        .then(datos => imprimirHTML(datos.results)); 
 }
+function imprimirHTML(datos){
+    datos.forEach(usuario => {
+        console.log(usuario);
+        const li = document.createElement('li');
 
-console.log(cliente); 
+        const { name: {first}, name: { last }, picture: {medium}, nat } = usuario;
 
-// crear la variable 
-const nombreCliente = cliente.nombre;
+        li.innerHTML = `
+            Nombre: ${first} ${last}
+            País: ${nat}
+            imagen: <img src="${medium}">
+        `;
 
-let {nombre} = cliente;
-
-console.log(nombre);
+        document.querySelector('#app').appendChild(li);
+    });
+}
