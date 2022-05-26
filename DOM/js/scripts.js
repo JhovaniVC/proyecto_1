278afe5ad65d5a820ba6757f860f032cafefd85d
JhovaniVC <velazquezchialex@gmail.com>
2022-05-26 08:16:12 -0500

    Update scripts.js

--- DOM/js/scripts.js
+++ DOM/js/scripts.js
@@ -1,4 +1,4 @@
-console.log("1"); 
+// console.log("1"); 
 
 (function(){
   'use strict';
@@ -9,17 +9,42 @@ console.log("1");
     // var navegacion = document.getElementById('navegacion');
     // navegacion.style.display = "none";
 
-    var navegacion = document.getElementsByClassName('navegacion');
-    console.log(navegacion[0]);
+    //getElementsByClassName
+    // var navegacion = document.getElementsByClassName('navegacion');
+    // console.log(navegacion[0]);
 
-    var contenido = document.getElementsByClassName('contenido');
-    console.log(contenido[0]); 
+    // var contenido = document.getElementsByClassName('contenido');
+    // console.log(contenido[0]); 
 
-    contenido[0].style.display = 'none';
+    // contenido[0].style.display = 'none';
+
+    //getElementsByTagName
+    // var enlaces = document.getElementsByTagName('a');
+    // console.log(enlaces.length);
     
+    // for(var i = 0; i < enlaces.length; i++ ){
+    //   enlaces[i].setAttribute('tarjet', '_blank');
+    // }
+
+    // var enlacesSidebar = document.getElementById('sidebar').getElementsByTagName('a');
+    // console.log(enlacesSidebar);
+
+    // for(var i = 0; i < enlacesSidebar.length; i++){
+    //   enlacesSidebar[i].setAttribute('href', 'http://www.google.com');
+    // }
+
+    var logo = document.querySelector('.logo');
+    console.log(logo); 
+
+    var encabezado = document.querySelectorAll('h2, footer p '); 
+    console.log(encabezado); 
+
+    var enlaces = document.querySelector('a');
+    console.log(enlaces);
+
 
 
   });
   
 })();
-console.log("3"); 
+// console.log("3"); 
