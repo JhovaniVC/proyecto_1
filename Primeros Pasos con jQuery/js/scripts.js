9c9287613214049835283e3bbee92a8b4f15ce34
JhovaniVC <velazquezchialex@gmail.com>
2022-05-29 06:02:13 -0500

    cambios en el jQuery

--- Primeros Pasos con jQuery/js/scripts.js
+++ Primeros Pasos con jQuery/js/scripts.js
@@ -1,11 +1,51 @@
 $(function(){
   'use strict';
-  
+
+  $('div.logo img').addClass('activo');
+
+  $('.navegacion').show();
+  $('main article:first').addClass('activo');
+
+  $('.navegacion ul li a').on('click', function(e){
+    $('.navegacion ul li a').removeClass('activo');
+    e.preventDefault();
+    $(this).addClass('activo');
+  });
+
+  /*
+  $('#menu') .on('click', function(){
+    $('#navegacion').show();
+  });
+*/
+  /*
+  $('div.logo img').on('mouseenter', function(){
+    console.log("Sobre el logo");
+  });
+  $('div.logo img').on('mouseleave', function(){
+    console.log("Fuera del logo");
+  });
+  */
+  /*
+  $('div.logo img').on('click', function(){
+    $(this).remove();
+  });
+*/
+  /*
+  $('div.logo img').on('click', function(){
+    console.log("Haz hecho click en el logo");
+  });
+*/
+
+  /**
   $('main article:first').hide();
   /*
   var copia = $('main article:last').clone();
   $('main').prepend(copia);
-  */
+
   var copia = $('main article:last').clone();
   $(copia).prependTo('main');
+   */
+  
+
+
 });
