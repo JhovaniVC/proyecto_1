5a94dd9bf7ac87a9e9be4bf0d0db2facaddfdc3b
JhovaniVC <velazquezchialex@gmail.com>
2022-05-30 03:11:19 -0500

    finalice la practica jQuery

--- Primeros Pasos con jQuery/js/scripts.js
+++ Primeros Pasos con jQuery/js/scripts.js
@@ -1,6 +1,106 @@
 $(function(){
   'use strict';
 
+  var proximosViajes = ['Londres', 'Valencia', 'Madrid', 'Paris', 'Milan'];
+
+  $.each(proximosViajes, function(i, v){
+    if(i == 0){
+      $('aside').append('<h2>Próximos Viajes</h2>');
+    }
+    $('aside').append('<li>' + v + '</li>')
+  });
+
+  var viajesPorFecha = {
+    primero: 'Londres',
+    segundo: 'Valencia',
+    tercero: 'Madrid',
+    cuarto: 'Paris',
+    quinto: 'Milan'
+  };
+  console.log(viajesPorFecha);
+
+  $.each(viajesPorFecha, function(i, v){
+    $('aside').append('<li>' + i + ' - ' + v + '</li>');
+  });
+
+  // $('aside').load('promociones.html');
+
+  /*
+  $('div.logo img').on('click', cargarAjax);
+
+  function cargarAjax(){
+    $.ajax('promociones.txt', {
+      success: agregarContenido,
+      type: 'GET',
+      datatype: 'text'
+    });
+  }
+
+    function agregarContenido(data, status, jqxhr){
+      $('aside').text(data);
+      console.log(status);
+    }
+*/
+
+  /*
+  $('.logo img').click(function(){
+    $('main article:first').slideUp(1000);
+  });
+  $('aside').click(function(){
+    $('main article:first').slideDown(1000);
+  });
+*/
+  /*
+  $('.logo img').on('click', function(){
+    $(this).animate({'width': '200px'}, 1000);
+  });
+
+  $('main article img').on('mouseenter', aumentarImagen);
+  $('main article img').on('mouseleave', disminuirImagen);
+
+  function aumentarImagen(){
+    $(this).animate({'width': '100%'});
+  }
+  function disminuirImagen(){
+    $(this).animate({'width':'350px'});
+  }
+*/
+  /*
+  $('.navegacion').show();
+
+  $('.logo img').css({'width': '400px'});
+
+  $('main article h2').css({'color': '#db008d'});
+
+  $('aside').css({'background-color': '#e1e1e1',
+          'text-transform': 'uppercase',
+          'padding': '20px'
+});
+
+$('.navegacion ul li a').on('mouseenter', cambiarColor);
+
+function cambiarColor(){
+  $('.navegacion').css('background-color', 'red')
+}
+*/
+/*
+  $('main article:first img').on('click', function(){
+    $(this).attr('src', 'img/imagen_2.jpg');
+  });
+*/
+  /*
+  $('main').on({
+    click: function() {
+      $(this).addClass('activo fondorojo');
+    }, mouseenter: function() {
+      $(this).addClass('fondorojo');
+    }, mouseleave: function(){
+      $(this).addClass('activo');
+
+    }
+  })
+*/
+  /*
   $('div.logo img').addClass('activo');
 
   $('.navegacion').show();
@@ -11,7 +111,7 @@ $(function(){
     e.preventDefault();
     $(this).addClass('activo');
   });
-
+*/
   /*
   $('#menu') .on('click', function(){
     $('#navegacion').show();
